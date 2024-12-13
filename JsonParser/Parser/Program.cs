29b55630e0533884df3493f7c4921d56625179a1
Muneeb Mughal <muneeb.devp@gmail.com>
2024-12-13 15:01:35 +0500

    Add command line argument parsing to patser

--- JsonParser/Parser/Program.cs
+++ JsonParser/Parser/Program.cs
@@ -1,19 +1,31 @@
 ﻿using Parser;
+using System.Linq;
 
-var directory = Directory.GetDirectories("Parser.Tests/tests");
-
-
-// var lexer = new Lexer("null"); 
-//  var parser = new Parser.Parser(lexer);
-//  
-//  var result = parser.Parse();
-//
-// Console.WriteLine(result);
-
-// while (true)
-// {
-//     var token = lexer.Lex();
-//     Console.WriteLine(token);
-//     if (token.Kind == TokenKind.EOF) break;
-// }
+try
+{
+  var jsonStr = Environment.GetCommandLineArgs().Skip(1).FirstOrDefault();
+  
+  if (string.IsNullOrEmpty(jsonStr))
+  {
+    Console.Error.WriteLine("No JSON string provided");
+    Environment.Exit(1);
+  }
+  
+  var lexer = new Lexer(jsonStr);
+  var parser = new Parser.Parser(lexer);
+  var result = parser.Parse();
+  
+  Console.WriteLine("Valid JSON string");
+  Environment.Exit(0);
+}
+catch (FormatException ex)
+{
+  Console.Error.WriteLine($"Invalid JSON string: \n{ex.Message}");
+  Environment.Exit(1);
+}
+catch (Exception ex)
+{
+  Console.Error.WriteLine($"An error occurred: \n{ex.Message}");
+  Environment.Exit(1);
+}
 
