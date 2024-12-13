b22d6d4ea0d17ce4070d3d9f397dccef3e37e86d
Muneeb Mughal <muneeb.devp@gmail.com>
2024-12-13 14:32:20 +0500

    Added test cases for JSON parser

--- JsonParser/Parser.Tests/LexerTests.cs
+++ JsonParser/Parser.Tests/LexerTests.cs
@@ -1,86 +1,147 @@
 namespace Parser.Tests;
 
 [TestFixture]
-public class ParserTests
+public class LexerTests
 {
      [Test]
     public void Lex_ShouldReturnOpenCurlyBraceToken()
     {
-        var parser = new Parser("{");
+        var parser = new Lexer("{");
         var token = parser.Lex();
-        Assert.That(token.Kind, Is.EqualTo(TokenKind.OpenCurlyBrace));
-        Assert.That(token.Literal, Is.EqualTo('{'));
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.OpenCurlyBrace));
+            Assert.That(token.Literal, Is.EqualTo('{'));
+        });
     }
 
     [Test]
     public void Lex_ShouldReturnCloseCurlyBraceToken()
     {
-        var parser = new Parser("}");
+        var parser = new Lexer("}");
         var token = parser.Lex();
-        Assert.That(token.Kind, Is.EqualTo(TokenKind.CloseCurlyBrace));
-        Assert.That(token.Literal, Is.EqualTo('}'));
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.CloseCurlyBrace));
+            Assert.That(token.Literal, Is.EqualTo('}'));
+        });
     }
 
     [Test]
     public void Lex_ShouldReturnCommaToken()
     {
-        var parser = new Parser(",");
+        var parser = new Lexer(",");
         var token = parser.Lex();
-        Assert.That(token.Kind, Is.EqualTo(TokenKind.Comma));
-        Assert.That(token.Literal, Is.EqualTo(','));
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.Comma));
+            Assert.That(token.Literal, Is.EqualTo(','));
+        });
     }
 
     [Test]
     public void Lex_ShouldReturnColonToken()
     {
-        var parser = new Parser(":");
+        var parser = new Lexer(":");
         var token = parser.Lex();
-        Assert.That(token.Kind, Is.EqualTo(TokenKind.Colon));
-        Assert.That(token.Literal, Is.EqualTo(':'));
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.Colon));
+            Assert.That(token.Literal, Is.EqualTo(':'));
+        });
     }
 
     [Test]
     public void Lex_ShouldReturnOpenSquareBracketToken()
     {
-        var parser = new Parser("[");
+        var parser = new Lexer("[");
         var token = parser.Lex();
-        Assert.That(token.Kind, Is.EqualTo(TokenKind.OpenSquareBracket));
-        Assert.That(token.Literal, Is.EqualTo('['));
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.OpenSquareBracket));
+            Assert.That(token.Literal, Is.EqualTo('['));
+        });
     }
 
     [Test]
     public void Lex_ShouldReturnCloseSquareBracketToken()
     {
-        var parser = new Parser("]");
+        var parser = new Lexer("]");
         var token = parser.Lex();
-        Assert.That(token.Kind, Is.EqualTo(TokenKind.CloseSquareBracket));
-        Assert.That(token.Literal, Is.EqualTo(']'));
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.CloseSquareBracket));
+            Assert.That(token.Literal, Is.EqualTo(']'));
+        });
     }
 
     [Test]
     public void Lex_ShouldReturnStringLiteralToken()
     {
-        var parser = new Parser("\"hello\"");
+        var parser = new Lexer("\"hello\"");
         var token = parser.Lex();
-        Assert.That(token.Kind, Is.EqualTo(TokenKind.StringLiteral));
-        Assert.That(token.Literal, Is.EqualTo("hello"));
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.StringLiteral));
+            Assert.That(token.Literal, Is.EqualTo("hello"));
+        });
     }
 
     [Test]
     public void Lex_ShouldReturnNumberLiteralToken()
     {
-        var parser = new Parser("123.45");
+        var parser = new Lexer("123.45");
         var token = parser.Lex();
-        Assert.That(token.Kind, Is.EqualTo(TokenKind.NumberLiteral));
-        Assert.That(token.Literal, Is.EqualTo(123.45));
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.NumberLiteral));
+            Assert.That(token.Literal, Is.EqualTo(123.45));
+        });
     }
 
     [Test]
+    public void Lex_ShouldReturnBooleanLiteralToken()
+    {
+        var parser = new Lexer("true");
+        var token = parser.Lex();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.BooleanLiteral));
+            Assert.That(token.Literal, Is.EqualTo(true));
+        });
+    }
+    
+    [Test]
     public void Lex_ShouldReturnEOFToken()
     {
-        var parser = new Parser("");
+        var parser = new Lexer("            ");
         var token = parser.Lex();
-        Assert.That(token.Kind, Is.EqualTo(TokenKind.EOF));
-        Assert.That(token.Literal, Is.EqualTo('\0'));
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.EOF));
+            Assert.That(token.Literal, Is.EqualTo('\0'));
+        });
+    }
+    
+    [Test]
+    public void Lex_ShouldReturnNullToken()
+    {
+        var lexer = new Lexer("null");
+        var token = lexer.Lex();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(token.Kind, Is.EqualTo(TokenKind.Null));
+            Assert.That(token.Literal, Is.Null);
+        });
     }
 }
--- JsonParser/Parser.Tests/ParserTests.cs
+++ JsonParser/Parser.Tests/ParserTests.cs
@@ -1,6 +1,141 @@
 namespace Parser.Tests;
 
+[TestFixture]
 public class ParserTests
 {
+    [Test]
+    public void Parse_StringLiteral_ReturnsString()
+    {
+        var lexer = new Lexer("\"hello\"");
+        var parser = new Parser(lexer);
+        var result = parser.Parse();
+        
+        Assert.That(result, Is.EqualTo("hello"));
+    }
+
+    [Test]
+    public void Parse_NumberLiteral_ReturnsNumber()
+    {
+        var lexer = new Lexer("123.45");
+        var parser = new Parser(lexer);
+        var result = parser.Parse();
+        
+        Assert.That(result, Is.EqualTo(123.45));
+    }
+
+    [Test]
+    public void Parse_BooleanLiteral_ReturnsBoolean()
+    {
+        var lexer = new Lexer("true");
+        var parser = new Parser(lexer);
+        var result = parser.Parse();
+        
+        Assert.That(result, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void Parse_NullLiteral_ReturnsNull()
+    {
+        var lexer = new Lexer("null");
+        var parser = new Parser(lexer);
+        var result = parser.Parse();
+        
+        Assert.That(result, Is.EqualTo(null));
+    }
+
+    [Test]
+    public void Parse_EmptyArray_ReturnsEmptyList()
+    {
+        var lexer = new Lexer("[]");
+        var parser = new Parser(lexer);
+        var result = parser.Parse();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.InstanceOf<List<object>>());
+            Assert.That(((List<object>)result), Is.Empty);
+        });
+    }
+
+    [Test]
+    public void Parse_ArrayWithValues_ReturnsList()
+    {
+        var lexer = new Lexer("[\"hello\", 123, true, null]");
+        var parser = new Parser(lexer);
+        var result = parser.Parse();
+        var list = (List<object>)result;
+
+        Assert.That(list, Has.Count.EqualTo(4));
+        Assert.Multiple(() =>
+        {
+            Assert.That(list[0], Is.EqualTo("hello"));
+            Assert.That(list[1], Is.EqualTo(123.0));
+            Assert.That(list[2], Is.EqualTo(true));
+            Assert.That(list[3], Is.EqualTo(null));
+        });
+    }
+
+    [Test]
+    public void Parse_EmptyObject_ReturnsEmptyDictionary()
+    {
+        var lexer = new Lexer("{}");
+        var parser = new Parser(lexer);
+        var result = parser.Parse();
+        Assert.That(result, Is.InstanceOf<Dictionary<string, object>>());
+        Assert.That((Dictionary<string, object>)result, Is.Empty);
+    }
+
+    [Test]
+    public void Parse_ObjectWithValues_ReturnsDictionary()
+    {
+        var lexer = new Lexer("{\"key1\": \"value1\", \"key2\": 123, \"key3\": true, \"key4\": null}");
+        var parser = new Parser(lexer);
+        var result = parser.Parse();
+        var dict = (Dictionary<string, object>)result;
+
+        Assert.That(dict, Has.Count.EqualTo(4));
+        Assert.Multiple(() =>
+        {
+            Assert.That(dict["key1"], Is.EqualTo("value1"));
+            Assert.That(dict["key2"], Is.EqualTo(123.0));
+            Assert.That(dict["key3"], Is.EqualTo(true));
+            Assert.That(dict["key4"], Is.EqualTo(null));
+        });
+    }
+
+    [Test]
+    public void Parser_RunAllTests()
+    {
+
+        string projectRoot = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+
+        foreach (var testDir in Directory.EnumerateDirectories(Path.Join(projectRoot, "tests")))
+        {
+            foreach (var file in Directory.EnumerateFiles(testDir))
+            {
+                if (file.Contains("invalid"))
+                {
+                    var input = File.ReadAllText(file);
+                    
+                    if (string.IsNullOrEmpty(input)) continue;
+                    
+                    var lexer = new Lexer(input);
+                    var parser = new Parser(lexer);
+                    Assert.That(() => parser.Parse(), Throws.Exception);
+                }
+                else if (file.Contains("valid"))
+                {
+                    var input = File.ReadAllText(file);
+                    var lexer = new Lexer(input);
+                    var parser = new Parser(lexer);
+                    var result = parser.Parse();
+                    Assert.That(result, Is.Not.Null);
+                }
+            }
+        }
+
+
+        Console.WriteLine(projectRoot);
+    }
     
 }
